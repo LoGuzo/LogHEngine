54144103bd422c3f2b8c73f4c26c6b824efb505c
LoGuzo <whcksdn16@gmail.com>
2024-08-13 02:06:24 +0900

    Create Static lib and connect project

--- LogHMainFolder/Editor_Window/main.cpp
+++ LogHMainFolder/Editor_Window/main.cpp
@@ -4,6 +4,10 @@
 #include "framework.h"
 #include "main.h"
 
+#include "..\\LogHEngine_Source\\LogHApplication.h"
+
+Application app;
+
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
@@ -42,7 +46,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
+    while (true)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    /*// 기본 메시지 루프입니다:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -50,7 +73,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
-    }
+    }*/
 
     return (int) msg.wParam;
 }
